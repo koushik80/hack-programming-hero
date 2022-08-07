a0077915b3d1442fed9eb6a47fac92d09b11100b
koushik <koushik.dey@live.com>
2022-08-07 14:56:26 +0300

    map-milestone

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@ const milestonesData = JSON.parse(data).data;
 // load course milestones data
 
 function loadMilestones() {
-    
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `${milestonesData.map(function (milestone) {
+        return `<div class="milestone border-b">
+            <div class="flex">
+              <div class="checkbox"><input type="checkbox" /></div>
+              <div>
+                <p>
+                  Milestone 1 name
+                  <span><i class="fas fa-chevron-down"></i></span>
+                </p>
+              </div>
+            </div>
+            <div class="hidden_panel">
+              <div class="module border-b">
+                <p>Module Name</p>
+              </div>
+            </div>
+          </div>`
+    })}`;
 }
+
+
+loadMilestones();
