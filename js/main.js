e82e94c17b53542487249de38fbc8098c0f7f308
koushik <koushik.dey@live.com>
2022-08-07 18:41:05 +0300

    mstone-donelist,img,item

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ const milestonesData = JSON.parse(data).data;
 function loadMilestones() {
     const milestones = document.querySelector('.milestones');
 
-    milestones.innerHTML = `${milestonesData.map(function (milestone) {
-        return `<div class="milestone border-b">
+    milestones.innerHTML = `${milestonesData
+        .map(function (milestone) {
+        return `<div class="milestone border-b" id="${milestone._id}">
             <div class="flex">
-              <div class="checkbox"><input type="checkbox" /></div>
+              <div class="checkbox"><input type="checkbox"
+              onclick="markMilestone(this, ${milestone._id})"/></div>
               <div onclick="openMilestone(this, ${milestone._id})">
                 <p>
                   ${milestone.name}
@@ -19,7 +21,7 @@ function loadMilestones() {
             <div class="hidden_panel">
               ${milestone.modules.map(function (module) {
                   return `<div class="module border-b">
-                <p>${module.name}</p>
+                  <p>${module.name}</p>
               </div>`;
               })
             .join("")}
@@ -29,26 +31,27 @@ function loadMilestones() {
         .join("")}`;
 
 }
-    function openMilestone(milestoneElement, id) {
-        const currentPanel = milestoneElement.parentNode.nextElementSibling;
-        const shownPanel = document.querySelector(".show");
-        const active = document.querySelector(".active");
+function openMilestone(milestoneElement, id) {
+    const currentPanel = milestoneElement.parentNode.nextElementSibling;
+    const shownPanel = document.querySelector(".show");
+    const active = document.querySelector(".active");
 
         // first remove previous active class if any [other than the clicked one]
-        if (active && !milestoneElement.classList.contains("active")) {
-            active.classList.remove("active");
-        }
+    if (active && !milestoneElement.classList.contains("active")) {
+      active.classList.remove("active");
+    }
 
-        // toggle current clicked one
-        milestoneElement.classList.toggle("active");
+    // toggle current clicked one
+    milestoneElement.classList.toggle("active");
 
-        // first hide previous panel if open [other than the clicked element]
-        if(!currentPanel.classList.contains("show") && shownPanel)
+    // first hide previous panel if open [other than the clicked element]
+    if (!currentPanel.classList.contains("show") && shownPanel)
         shownPanel.classList.remove("show");
-       // toggle current element
-        currentPanel.classList.toggle("show");
 
-        showMilestone(id);
+    // toggle current element
+    currentPanel.classList.toggle("show");
+
+    showMilestone(id);
     }
 
 function showMilestone(id) {
@@ -70,7 +73,24 @@ milestoneImage.onload = function () {
     this.style.opacity = "1";
 };
 
+function markMilestone() {
+    const doneList = document.querySelector(".doneList");
+    const milestonesList = document.querySelector(".milestones");
+    const item = document.getElementById(id);
 
+    if (checkbox.checked) {
+        // mark as done
+        milestonesList.removeChild(item);
+        doneList.appendChild(item);
+    } else {
+        // back to main list
+        milestonenessList.appendChild(item);
+        doneList.removeChild(item);
 
+        // task - do the sorting
+        //reload list
+    }
+
+}
 
 loadMilestones();
